06506bc2be08ff440c6d80ac8ea13a076970af66
davidh0ng <djhong83@gmail.com>
2025-09-18 17:27:35 -0700

    made minor change to fixing autoscaling issue in create-env.sh

--- Module-05/create-env.sh
+++ Module-05/create-env.sh
@@ -113,7 +113,7 @@ echo 'Creating Auto Scaling Group...'
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/autoscaling/create-auto-scaling-group.html
 aws autoscaling create-auto-scaling-group \
   --auto-scaling-group-name ${13} \
-  --launch-template "LaunchTemplateID=${LAUNCHTEMPLATEID},Version=1" \
+  --launch-template "LaunchTemplateName=${12},Version=1" \
   --min-size ${14} \
   --max-size ${15} \
   --desired-capacity ${16} \
